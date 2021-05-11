4d179fc59ad086c804fd501cfe882e7c075db635
Anita Lipsky <anita@purplebugs.com>
2021-05-11 12:49:56 +0200

    bugfix: Elasticsearch request helper, cleanup up web component loop

--- static/rating-counter.js
+++ static/rating-counter.js
@@ -1,14 +1,6 @@
 import { html, css, LitElement } from "https://unpkg.com/lit-element?module";
 
 export class RatingCounter extends LitElement {
-  static get styles() {
-    return css`
-      p {
-        color: blue;
-      }
-    `;
-  }
-
   static get properties() {
     return {
       rating: { type: String },
@@ -23,17 +15,13 @@ export class RatingCounter extends LitElement {
   }
 
   render() {
-    let output = this.rating;
+    let output = "";
 
-    if (this.counter === "1") {
-      output = `${this.rating}`;
-    } else if (this.counter === "2") {
-      output = `${this.rating} ${this.rating}`;
-    } else if (this.counter === "3") {
-      output = `${this.rating} ${this.rating} ${this.rating}`;
-    } else {
-      output = "No rating";
+    let i = 0;
+    for (i = 0; i < this.counter; i++) {
+      output += `${this.rating}`;
     }
+
     return html`<span> ${output} </span>`;
   }
 }
